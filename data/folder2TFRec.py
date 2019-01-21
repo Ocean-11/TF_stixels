73535a49a153d1dabda7959f3b6eedfa1b559aff
Ocean-11 <ran.zaslavsky@icloud.com>
2019-01-21 16:51:55 +0200

    fix control images display issue

--- data/folder2TFRec.py
+++ data/folder2TFRec.py
@@ -1,6 +1,6 @@
 '''
 *
-* dir_to_tfrecords
+* folder2TFRec()
 *
 * Purpose: the module receives a directory name, creates train/valid/test/control/meta_data
 *          folders, and scans it's "annotated" folder, translating annotated images into
@@ -45,19 +45,20 @@ from tkinter import filedialog
 import csv
 from TF_stixels.code.model import params
 import random
+import shutil
 
+TRAIN_IMAGES_RATIO = 80
+VALID_IMAGES_RATIO = 15
 
 class Frame2StxTfrecords:
     def __init__(self, frame_path, GT_file_path, writer,control_dir, stixel_width, frame_type):
         ' Define stixels dimensions'
         self.stx_w = stixel_width  # stixel width
-        #self.stx_w = 24  # stixel width
         self.stx_half_w = int(self.stx_w/2)
-        #print('stixel half width = {}'.format(self.stx_half_w))
         self.stx_h = 370  # stixel height
         self.bin_pixels = 5  # stixel bins height
         self.stride = 5  # stride used to split to the frame
-        ' init internal data'
+        ' init internal data '
         self.frame_path = frame_path  # frame path
         self.GT_file_path = GT_file_path  # ground truth
         self.writer = writer
@@ -66,14 +67,15 @@ class Frame2StxTfrecords:
         self.frame_type = frame_type
 
         ' Read GT file (retain only first 2 columns) '
-        GT_data = pd.read_csv(GT_file_path, header=None)
+        if (GT_file_path.endswith('.csv')):
+            GT_data = pd.read_csv(GT_file_path, header=None)
+        else:
+            GT_data = pd.read_csv('/home/dev/PycharmProjects/stixel/TF_stixels/default_GT.csv', header=None)
+            #print('no CSV file given - using default')
         GT_df = pd.DataFrame(GT_data)
         GT_df = GT_df.iloc[:, 0:2] #getting rid of columns 3 & 4
         GT_df.columns = ["x","y"] # adding columns names
         self.frame_ground_truth = GT_df
-
-        #self.frame_ground_truth = pd.read_csv(GT_file_path, names=["x", "y"])
-
 
     def int64_feature(self, value):
         return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
@@ -103,14 +105,15 @@ class Frame2StxTfrecords:
     def create_stx(self, printCtrlImage):
         ' read current frame '
         img = mpimg.imread(self.frame_path)  # img is np.array of the frame
-        h, w, c = img.shape
-        #print('image dimensions: h={} w={} c={}'.format(h, w, c))
+        height, width, c = img.shape
+        #print('image dimensions: h={} w={} c={}'.format(height, width, c))
         x_start = self.frame_ground_truth.x.iat[0]
-        x_stop = self.frame_ground_truth.x.iat[-1]
+        x_stop = min(self.frame_ground_truth.x.iat[-1], width)
+        #x_stop = self.frame_ground_truth.x.iat[-1]
         annotated_w = x_stop - x_start + 1
-        #print('start={} stop={} w={}'.format(x_start, x_stop, annotated_w))
+        print('start={} stop={} w={}'.format(x_start, x_stop, annotated_w))
         num_stixels = int(((annotated_w - self.stx_w) / self.stride) + 1)
-        print('number of stixles to be generated {}'.format(num_stixels))
+        print('stixel width = {}, number of stixles to be generated {}'.format(self.stx_w, num_stixels))
         frame_name = os.path.basename(self.frame_path)
 
         ' display the current frame'
@@ -120,63 +123,47 @@ class Frame2StxTfrecords:
         for stixel in range(num_stixels):
 
             imName = '_'.join([os.path.splitext(frame_name)[0], 'stx', str(stixel).zfill(3)]) # RAN - 13-12
-            #print(imName)
+            #print('image name = ' + imName)
             #imName = '_'.join([self.output_prefix, os.path.splitext(frame_name)[0], 'stx', str(stixel).zfill(3)])
 
             i = self.stx_half_w + (stixel * 5) + x_start
             #i = 12 + (stixel * 5) + x_start
-            # print('\nstixel {} center = {}'.format(stixel, i))q
+            #print('\nstixel {} center = {}'.format(stixel, i))
             ' cut the lower image part (high y values)'
             if img.shape[0] == self.stx_h:
                 s = img[:, i - self.stx_half_w:i + self.stx_half_w, :]  # that's the stixel
+                print('diff_h not defined !!!!!!!!')
                 #s = img[:, i - 12:i + 12, :]  # that's the stixel
             else:
                 diff_h = img.shape[0] - self.stx_h
                 s = img[diff_h:, i - self.stx_half_w:i + self.stx_half_w, :]  # that's the stixel
-                #s = img[diff_h:, i - 12:i + 12, :]  # that's the stixel
+                # s = img[diff_h:, i - 12:i + 12, :]  # that's the stixel
 
             ' find the closest GT points '
             stx_GT_y = -1
             GT_point_ID_1 = next(x[0] for x in enumerate(self.frame_ground_truth.x) if x[1] > i)
             GT_point_ID_2 = max(GT_point_ID_1 - 1, 0)
 
-            '''
+
             print('GT for stixel {} points - ({},{}), ({},{})'.format(
                 stixel,
                 self.frame_ground_truth.x[GT_point_ID_2],
                 self.frame_ground_truth.y[GT_point_ID_2],
                 self.frame_ground_truth.x[GT_point_ID_1],
                 self.frame_ground_truth.y[GT_point_ID_1]))
-            '''
 
             ' interpolate the GT value'
             x_GT = [self.frame_ground_truth.x[GT_point_ID_2], self.frame_ground_truth.x[GT_point_ID_1]]
             y_GT = [self.frame_ground_truth.y[GT_point_ID_2], self.frame_ground_truth.y[GT_point_ID_1]]
             y_interp = scipy.interpolate.interp1d(x_GT, y_GT)
             stx_GT_y = int(y_interp(i))
-            # print('Ground truth result = {}'.format(stx_GT_y))
+            #print('Ground truth result = {}'.format(stx_GT_y))
 
             'translate from absolute GT coordinates to stixel coordinates and translate to label'
             stx_GT_y -= diff_h
-            if (stx_GT_y < -5):
-                ' border higher than 5 pixels - skip '
-                stx_GT_y = 0
-                for_use = 0
-            elif (stx_GT_y < 0):
-                ' label as "0" '
-                stx_GT_y = 0
-                for_use = 1
-            else:
-                for_use = 1
-
-            '''
-            if (stx_GT_y < 0): # RAN - 31-12-18
-                'no border in stixel range'
-                stx_GT_y = 0
-                for_use = 0
-            else:
-                for_use = 1
-            '''
+            for_use = 1 # use all Stixels
+            if (stx_GT_y < 0):
+                stx_GT_y = 0 # label as "0"
 
             stx_label = np.floor_divide(stx_GT_y, 5)
             #print('stixel GT value = {}, use={}'.format(stx_label,for_use))
@@ -184,18 +171,28 @@ class Frame2StxTfrecords:
             ' display the GT on the stixel or on the original image '
             # self.plot_stx_GT(imName,s,stx_label)
             if stx_label == 0:
+                plt.plot(i, diff_h + (stx_label * 5) + 2, marker='o', markersize=2, color="black")
+            else:
+                plt.plot(i, diff_h + (stx_label * 5) + 2, marker='o', markersize=2, color="red")
+            plt.draw()
+
+            '''
+            if stx_label == 0:
                 plt.plot(x_start + i, diff_h + (stx_label * 5) + 2, marker='o', markersize=2, color="black")
             else:
                 plt.plot(x_start + i, diff_h + (stx_label * 5) + 2, marker='o', markersize=2, color="red")
             plt.draw()
+            '''
 
             ' dilute the amount of lower end border Stixels by a factor of 1:4'
+            '''
             dilution_factor = 2
             if (stx_label == 73):
                 p_label = random.randint(1,dilution_factor + 1)
                 #print('prob = {}'.format(p_label))
                 if (p_label>1):
                     for_use = 0
+            '''
 
             ' save the stixel only if there is a boundary within the stixel'
             if for_use == 1:
@@ -210,19 +207,6 @@ class Frame2StxTfrecords:
                 tf_example = self.create_tf_example(contents, stx_label)
                 self.writer.write(tf_example.SerializeToString())
 
-            '''
-            ' Apend the new stixel label'
-            self.labels.append([imName, stx_label, for_use, self.frame_type])
-
-            ' save a tfrecord file'
-            img_for_tfrec = Image.fromarray(s)
-            with io.BytesIO() as output:
-                img_for_tfrec.save(output, format="PNG")
-                contents = output.getvalue()
-            tf_example = self.create_tf_example(contents, stx_label)
-            self.writer.write(tf_example.SerializeToString())
-            '''
-
         # print('labels are: {}'.format(list(zip(*self.labels))[1]))
 
         if (printCtrlImage):
@@ -230,7 +214,7 @@ class Frame2StxTfrecords:
 
             control_imagepath = (self.control_dir + '/'+ os.path.basename(self.frame_path)).replace('.png', '_labeled.png')
             control_imagepath = control_imagepath.replace('.jpg', '_labeled.png')
-            print('stixels created - saving control image to ' + control_imagepath)
+            #print('stixels created - saving control image to ' + control_imagepath)
             plt.savefig(control_imagepath)
 
         plt.close('all')
@@ -253,26 +237,24 @@ def main(data_dir, stixel_width, isControl = True):
     else:
         print('Folder name: ' + object_name + 'images to be converted - {}'.format(num_images))
 
-
     # Create required folders
-    if not os.path.exists(data_dir + '/train') and not os.path.isdir(data_dir + '/train'):
-        os.mkdir(data_dir + '/train')
-
-    if not os.path.exists(data_dir + '/valid') and not os.path.isdir(data_dir + '/valid'):
-        os.mkdir(data_dir + '/valid')
-
-    if not os.path.exists(data_dir + '/test') and not os.path.isdir(data_dir + '/test'):
-        os.mkdir(data_dir + '/test')
-
-    if not os.path.exists(data_dir   + '/control') and not os.path.isdir(data_dir + '/control'):
-        os.mkdir(data_dir + '/control')
-
-    if not os.path.exists(data_dir + '/meta_data') and not os.path.isdir(data_dir + '/meta_data'):
-        os.mkdir(data_dir + '/meta_data')
+    data_dir = data_dir+'/W'+ str(stixel_width)
+    print(data_dir)
+    if os.path.exists(data_dir) and os.path.isdir(data_dir):
+        shutil.rmtree(data_dir)
+    os.mkdir(data_dir)
+    folders = {'train', 'valid', 'test', 'control', 'meta_data'}
+    for folder_ in folders:
+        new_folder = data_dir + '/' + folder_
+        '''
+        if os.path.exists(new_folder) and os.path.isdir(new_folder):
+            shutil.rmtree(new_folder)
+        '''
+        os.mkdir(new_folder)
+        print('create ' + new_folder)
 
     # Create object labels list
     object_labels = []
-
 
     # Create TFRecord writers
     train_writer = tf.python_io.TFRecordWriter(data_dir + '/train/' + object_name + '_W' + str(stixel_width) + '.tfrecord')
@@ -280,16 +262,9 @@ def main(data_dir, stixel_width, isControl = True):
     print('Opening writers for ' + object_name + ' train/valid records')
     #object_stixels_list = []
 
-    # Create this object's images control directory
+    # Define images control directory
     control_dir = data_dir + '/control'
-    '''
-    control_dir = data_dir + '/tfrecords/control/' + object
-    if isControl:
-        if not os.path.exists(control_dir) and not os.path.isdir(control_dir):
-            os.mkdir(control_dir)
-            print('created images annotations control dir - ' + control_dir)
-            '''
-
+    capture_train_image = True
 
     # Go through object images
     for index in range(num_images):
@@ -297,30 +272,30 @@ def main(data_dir, stixel_width, isControl = True):
 
         # Make sure there's an image - otherwise move to next image
         frame_path = (i.split('.')[0]) + '.jpg'
-        if os.path.exists(frame_path):
-            print('parse: ' + frame_path)
-        else:
+        if not(os.path.exists(frame_path)):
             print(frame_path + ' does not exist!')
             continue
 
-        # Let's make 80% train, 15% for validation & 5% for test
+        # Split images between train/validation/test
+        type_val = random.randint(1, 101)
         isTestImage = False
-        frame_type = 'train'
-        if index < num_images * 0.8:
+        if type_val<=TRAIN_IMAGES_RATIO:
             writer = train_writer
-            #print('write to test writer')
-        elif index < num_images * 0.95:
+            frame_type = 'train'
+            print('parse: ' + frame_path + '->train')
+        elif type_val<=(TRAIN_IMAGES_RATIO+VALID_IMAGES_RATIO):
             writer = valid_writer
             frame_type = 'valid'
-            #print('write to valid writer')
+            print('parse: ' + frame_path + '->valid')
         else:
             # Create new test writer for each image
+            frame_type = 'test'
             frame_name = os.path.basename(i.split('.')[0])
-            test_writer = tf.python_io.TFRecordWriter(data_dir + '/test/' + object_name + ' ' + frame_name + '_test_W' + str(stixel_width) + '.tfrecord')
+            test_writer = tf.python_io.TFRecordWriter(
+                data_dir + '/test/' + object_name + ' ' + frame_name + '_test_W' + str(stixel_width) + '.tfrecord')
             writer = test_writer
             isTestImage = True
-            frame_type = 'test'
-
+            print('parse: ' + frame_path + '->test')
 
         # parse the image and save it to TFrecord
         f_to_stx = Frame2StxTfrecords(frame_path, i, writer,control_dir, stixel_width, frame_type)
@@ -332,7 +307,7 @@ def main(data_dir, stixel_width, isControl = True):
             isTestImage = False
 
         # Save the 1st train image to test directory for further validation
-        if index == 0:
+        if (frame_type == 'train') and (capture_train_image):
             # Create new test writer
             frame_name = os.path.basename(i.split('.')[0])
             test_writer = tf.python_io.TFRecordWriter(
@@ -341,6 +316,7 @@ def main(data_dir, stixel_width, isControl = True):
                 str(stixel_width) + '.tfrecord')
             f_to_stx = Frame2StxTfrecords(frame_path, i, test_writer, control_dir, stixel_width, frame_type)
             f_to_stx.create_stx(isControl)
+            capture_train_image = False
             test_writer.close()
 
     train_writer.close()
@@ -365,6 +341,7 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.withdraw()  # we don't want a full GUI, so keep the root window from appearing
     data_dir = filedialog.askdirectory(initialdir='/media/vision/DataRepo')
+    root.destroy()
 
     print('Convert to TFrecords all annotated images within - ' + data_dir + ':')
 
