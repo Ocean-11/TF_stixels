4a6430d9adaf738ea39b67645ca77bf72b1aa031
Ocean-11 <ran.zaslavsky@icloud.com>
2019-02-04 18:15:40 +0200

    create dataset.py changed to create_new_dataset.py

--- data/create_new_dataset.py
+++ data/create_new_dataset.py
@@ -16,17 +16,11 @@
 * Written by: Ran Zaslavsky 30-12-2018
 '''
 
-
-
 import glob, os
 import shutil
 import pandas as pd
-import numpy as np
-import tkinter as tk
-from tkinter import filedialog
 
-
-def main(data_dir, target_dir):
+def main(data_dir, target_dir, stixel_width):
 
     # Gather file paths to all annotated images within the target directory (and subfolders)
     object_dirs = glob.glob(data_dir + '/*')
@@ -35,25 +29,22 @@ def main(data_dir, target_dir):
     for d in object_dirs:
         objects[d.split('/')[-1]] = glob.glob(d + '/*.csv') # Take the last subfolder name as the key
         #objects[d.split('/')[1]] = glob.glob(d + '/*.csv')
-        print(d)
+        print('copy from: ' + d)
 
     # Create train/valid/test directories to store our TFRecords
+    print('target directory = ' + target_dir)
     if not os.path.exists(target_dir) and not os.path.isdir(target_dir):
         os.mkdir(target_dir)
 
-    if not os.path.exists(target_dir+'/train') and not os.path.isdir(target_dir+'/train'):
-        os.mkdir(target_dir+'/train')
-
-    if not os.path.exists(target_dir+'/valid') and not os.path.isdir(target_dir+'/valid'):
-        os.mkdir(target_dir+'/valid')
-
-    if not os.path.exists(target_dir + '/test') and not os.path.isdir(target_dir + '/test'):
-        os.mkdir(target_dir + '/test')
-
-    if not os.path.exists(target_dir + '/meta_data') and not os.path.isdir(target_dir + '/meta_data'):
-        os.mkdir(target_dir + '/meta_data')
+    # Create required folders
+    folders = {'train', 'valid', 'test', 'meta_data'}
+    for folder_ in folders:
+        new_folder = target_dir + '/' + folder_
+        if not os.path.exists(new_folder) and not os.path.isdir(new_folder):
+            os.mkdir(new_folder)
 
     object_names = list(objects.keys())
+    width_alias = '/W' + str(stixel_width)
     copy_dir_list = ['/train', '/valid', '/test']
     master_filename = os.path.join(target_dir + '/meta_data', "meta_data.csv") # defines the master CSV file name
 
@@ -61,20 +52,19 @@ def main(data_dir, target_dir):
     for object in object_names:
         object_path = os.path.join(data_dir, object)
         for dir_name in copy_dir_list:
-            tfrec_files = glob.glob(object_path + dir_name + '/*.tfrecord')
+            tfrec_files = glob.glob(object_path + width_alias + dir_name + '/*.tfrecord')
             for file in tfrec_files:
+                print('copy ' + file + ' to ' + dir_name)
                 shutil.copy(file, target_dir + dir_name)
 
-    # Copy CSV files & create a master CSV file    
+
+    # Copy CSV files & create a master CSV file
     fout = open(master_filename, "a")
-    #headers = []
-    #headers.append(['frame', 'label', 'for_use', 'frame_type'])
-    #headers = 'frame,label,for_use,frame_type'
-    #fout.write(headers)
     for object in object_names:
         object_path = os.path.join(data_dir, object)
-        data_files = glob.glob(object_path + '/meta_data' + '/*.csv')
+        data_files = glob.glob(object_path + width_alias + '/meta_data' + '/*.csv')
         for file_ in data_files:
+            print('copy ' + file_)
             shutil.copy(file_, target_dir + '/meta_data')
             # Copy CSV lines to master CSV
             for line in open(file_):
@@ -89,23 +79,13 @@ def main(data_dir, target_dir):
     table.to_excel(writer)
     writer.save()
 
-
     #meta_data.extend() # append the new frame labels data
 
 if __name__ == '__main__':
 
-    ' when executed as a script, open a GUI window to select the presented TFrecord file '
-    '''
-    root = tk.Tk()
-    root.withdraw()  # we don't want a full GUI, so keep the root window from appearing
-    data_dir = filedialog.askdirectory()
-    '''
-
     data_dir = '/media/vision/DataRepo'
-    #data_dir = '/media/vision/temp_DataRepo'
-    target_dir = '/media/vision/Datasets/Dataset_5'
-
-    #data_dir = 'annotated' # TEMPORARY !!!!!!
+    target_dir = '/media/vision/Datasets/Dataset_14'
+    stixel_width = 36
     print('Extract files from DataRepo - ' + data_dir + ':')
 
-    main(data_dir, target_dir)
+    main(data_dir, target_dir, stixel_width )
